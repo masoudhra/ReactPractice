0a96593dece64110e48474833a2f7f4253aab091
Masoud Ramezani <masoudhra@gmail.com>
2023-03-24 14:43:45 +1300

    add some practice about fetch and get data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import Child from './Child';
-import Child2 from './Child2';
-import SayHi from './SayHi';
 
-import ClassEvent from './ClassEvent';
-// import FunctionEvent from './FunctionEvent';
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      postData: [],
+    }
+  }
+
+  getPost = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+          .then((response) => response.json())
+          .then((json) => console.log(json));
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+          .then((response) => response.json())
+          .then((json) => this.setState({
+            postData: json
+          }));
+
+  }
+
+
   render() {
     return (
       <div>
-          <ClassEvent />
-          {/* <FunctionEvent /> */}
-          <Child />
-          <Child2 />
-          <SayHi sayHi={bool => bool ? "Hi" : "By"} />
+        <button onClick={this.getPost}>get post</button>
+        <h1>Posts: </h1>
+        {this.state.postData.map(post => <p key={post.id}>{post.title}</p>)}
       </div>
     );
   }
