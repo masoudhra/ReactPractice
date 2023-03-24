b71c306fbcaf73263fb3a010f03e0623cf5d53dd
Masoud Ramezani <masoudhra@gmail.com>
2023-03-24 15:23:57 +1300

    coment and practice fetch again for get

--- src/App.js
+++ src/App.js
@@ -1,3 +1,41 @@
+// import React, { Component } from 'react';
+
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       postData: [],
+//     }
+//   }
+
+//   getPost = () => {
+//     fetch('https://jsonplaceholder.typicode.com/posts/1')
+//           .then((response) => response.json())
+//           .then((json) => console.log(json));
+//   }
+//   componentDidMount() {
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//           .then((response) => response.json())
+//           .then((json) => this.setState({
+//             postData: json
+//           }));
+
+//   }
+
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.getPost}>get post</button>
+//         <h1>Posts: </h1>
+//         {this.state.postData.map(post => <p key={post.id}>{post.title}</p>)}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
 import React, { Component } from 'react';
 
 class App extends Component {
@@ -11,25 +49,24 @@ class App extends Component {
 
   getPost = () => {
     fetch('https://jsonplaceholder.typicode.com/posts/1')
-          .then((response) => response.json())
-          .then((json) => console.log(json));
+        .then((response) => response.json())
+        .then((json) => console.log(json));
   }
-  componentDidMount() {
+  componentDidMount (){
     fetch('https://jsonplaceholder.typicode.com/posts')
-          .then((response) => response.json())
-          .then((json) => this.setState({
-            postData: json
-          }));
-
+        .then((response) => response.json())
+        .then((json) =>this.setState({
+      postData: json
+  }));
   }
 
 
   render() {
     return (
       <div>
-        <button onClick={this.getPost}>get post</button>
-        <h1>Posts: </h1>
-        {this.state.postData.map(post => <p key={post.id}>{post.title}</p>)}
+          <button onClick={this.getPost}>get post</button>
+          <h1>Posts:</h1>
+          {this.state.postData.map(post => <p key={post.id}>{post.title}</p>)}
       </div>
     );
   }
