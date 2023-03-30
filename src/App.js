964f72673db4e522ba98d7b1ef316059856c72b7
Masoud Ramezani <masoudhra@gmail.com>
2023-03-30 14:43:17 +1300

    create route for components with react rputer dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import { Route } from "react-router-dom";
+
+import HomePage from './components/HomePage';
+import Blogs from './components/Blogs';
+import Products from './components/Products';
+import AboutUs from './components/AboutUs';
 
 class App extends Component {
     render() {
         return (
             <div>
+                <ul>
+                    <li><a href="/">HomePage</a></li>
+                    <li><a href="/blogs">Blogs</a></li>
+                    <li><a href="/products">Products</a></li>
+                    <li><a href="/aboutus">About Us</a></li>
+            
+                </ul>
+                <div>
+                    <Route path="/" component={HomePage} />
+                    <Route path="/blogs" component={Blogs} />
+                    <Route path="/products" component={Products} />
+                    <Route path="/aboutus" component={AboutUs} />
+                </div>
                 
             </div>
         );
