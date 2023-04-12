1ba7b9d89106067ea5f0cf83ed0353b242d16573
Masoud Ramezani <masoudhra@gmail.com>
2023-04-12 14:47:47 +1200

    use object in useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
-import Counter from './components/Counter';
+import Form from './components/Form';
 class App extends Component {
     render() {
         return (
             <div>
-                <Counter />
+
+                <Form />
+               
+
             </div>
         );
     }
