c139875dd25547294d58879e82e5fafdd2562fe3
Masoud Ramezani <masoudhra@gmail.com>
2023-04-12 14:58:52 +1200

    array in useState

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 
 const Form = () => {
 
-    const [data, setData] = useState({name: "", age: ""});
+    const [data, setData] = useState(["item"]);
 
 
     return (
         <div>
-            <input type="text" value={data.name} onChange={event => setData({...data, name: event.target.value})} />
-            <input type="number" value={data.age} onChange={event => setData({...data, age: event.target.value})} />
+            
+            <button onClick={() => setData([...data, "newItem"])}>Add</button>
 
             <p>{JSON.stringify(data)}</p>
         </div>
