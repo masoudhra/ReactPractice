06cbccb108f11a7817a0cba391bb753d60688d09
Masoud Ramezani <masoudhra@gmail.com>
2023-04-13 14:19:00 +1200

    practice useState ep1 again no

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+
+// const Form = () => {
+
+//     const [data, setData] = useState(["item"]);
+
+
+//     return (
+//         <div>
+            
+//             <button onClick={() => setData([...data, "newItem"])}>Add</button>
+
+//             <p>{JSON.stringify(data)}</p>
+//         </div>
+//     );
+// };
+
+// export default Form;
+import React, {useState} from 'react';
 
 const Form = () => {
+    const [number, setNumber] = useState(0)
 
-    const [data, setData] = useState(["item"]);
+    const clickHandler = () =>{
+        setNumber(prevNumber => prevNumber + 3)
+
+    }
 
 
     return (
         <div>
-            
-            <button onClick={() => setData([...data, "newItem"])}>Add</button>
 
-            <p>{JSON.stringify(data)}</p>
+            <button onClick={clickHandler}>plus 3</button>
+            <h1>{number}</h1>
         </div>
     );
 };
